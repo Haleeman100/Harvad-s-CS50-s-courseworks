a79302be4653ff4277a513c4659d46cd064152bc
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2023-07-24 20:12:08 +0100

    added method to capture the required shorter link

--- numb3rs/watch.py
+++ numb3rs/watch.py
@@ -6,3 +6,28 @@ one of the formats below. Assume that the value of src will be surrounded by dou
 And assume that the input will contain no more than one such URL. If the input does not contain
 any such URL at all, return None.
 """
+#<iframe src="http://www.youtube.com/embed/xvFZjo5PgG0"></iframe>
+import re
+
+def main():
+    s = input("HTML: ").strip()
+    r = parse(s)
+    print(r)
+
+def parse(s):
+    iframe_pattern = r'<iframe[^>]*src="https?://(?:www\.)?youtube\.com/embed/([^"]*)"[^>]*>'
+    matches = re.search(iframe_pattern, s, re.IGNORECASE)
+    if matches:
+        capture = matches.group(1)
+        #youtu_be_url = re.sub(r'www\.youtube\.com/', 'https://youtu.be/', youtube_code)
+        short_url = f"https://youtu.be/{capture}"
+        return short_url
+    else:
+        return None
+    ...
+
+
+...
+
+if __name__ == "__main__":
+    main()
