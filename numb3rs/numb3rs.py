cbf3d0180e89ae02c730ba43ccb1cce4f63a6ea0
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2023-07-24 12:56:09 +0100

    added validate method which validates ip addresses

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -10,4 +10,26 @@ as a str and then returns True or False, respectively, if that input is a valid 
 Structure numb3rs.py as follows, wherein you’re welcome to modify main and/or implement other functions
  as you see fit, but you may not import any other libraries. You’re welcome, but not required, to use re and/or sys.
 """
+import re
 
+def main():
+    ip = input("Ipv4 address: ").strip()
+    return validate(ip)
+
+# This function returns bool based on if the ip address address is right or wrong
+def validate(ip):
+    # 25[0-5] uses regex to check and see matching string from 250-255
+    # 2[0-4][0-9] checks 200-249
+    #[ 01]?[0-9] checks from 0-1 to 199
+    return bool(re.search(
+        r"^(25[0-5]|2[0-4][0-9]|[01]?[0-9]{1,2})\."
+        r"(25[0-5]|2[0-4][0-9]|[01]?[0-9]{1,2})\."
+        r"(25[0-5]|2[0-4][0-9]|[01]?[0-9]{1,2})\."
+        r"(25[0-5]|2[0-4][0-9]|[01]?[0-9]{1,2})$", ip
+    )
+
+    )
+
+if __name__ == "__main__":
+    result = main()
+    print(result)
