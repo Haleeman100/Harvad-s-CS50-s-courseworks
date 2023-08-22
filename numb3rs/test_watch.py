4cf2ec0cbfebea44d2dde55c1748e3d22c38bdc3
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2023-08-22 22:06:37 +0100

    added test codes

--- numb3rs/test_watch.py
+++ numb3rs/test_watch.py
@@ -3,5 +3,20 @@ import pytest
 from watch import parse
 
 
-def test_valid_youtube_links():
-    assert parse("") is True
+def test_valid_youtube_links_http():
+    html_input = '<iframe src="http://youtube.com/embed/xvFZjo5PgG0"></iframe>'
+    expected_output = 'https://youtu.be/xvFZjo5PgG0'
+    assert parse(html_input) == expected_output
+
+def test_valid_youtube_links_https():
+    html_input = '<iframe src="https://www.youtube.com/embed/xvFZjo5PgG0"></iframe>'
+    expected_output = 'https://youtu.be/xvFZjo5PgG0'
+    assert parse(html_input) == expected_output
+
+def test_no_valid_youtube_link():
+    html_input = '<iframe src="https://example.com"></iframe>'
+    assert parse(html_input) is None
+
+if __name__ == "__main__":
+    pytest.main()
+
