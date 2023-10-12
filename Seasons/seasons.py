298ddcc66c4b4fcde0a9c35eb9e0cbb8fd32c1bb
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2023-10-12 19:47:16 +0100

    Added a class and two  functions to convert numbers to words

--- Seasons/seasons.py
+++ Seasons/seasons.py
@@ -1,16 +1,33 @@
 
 
 class ListWords:
-    ...
+    def __init__(self):
+        self.list_word = ["Zero","One", "Two", "Three", "Four", "Five", "Six", "Seven",
+                      "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen",
+                      "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty",
+                      "Thirty", "Fourty", "Fifty", "Sisty", "Seventy", "Eighty", "Ninety",  ]
+    
+    def get_words(self, num):
+        if num in self.list_word:
+            return self.list_word[num]
+        
+        elif 0 <= num <= 20:
+            return self.list_word[num]
+        
+        elif 20 <= num <= 99:
+            
+            ...
+...
 
 
 
 
 
 def main():
+    var = (22//10)
+    print (var) 
     ...
 
 
-
-if main == __name__:
+if __name__ == "__main__":
     main()
