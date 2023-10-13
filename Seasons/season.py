7cd8c76cda9304593ace33dfb87968883e17168f
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2023-10-13 19:05:03 +0100

    create a class for converting date to age

--- Seasons/season.py
+++ Seasons/season.py
@@ -8,10 +8,16 @@ class Convert:
     def convert_number(self, number):
         if number >= 0:
             words = self.p.number_to_words(number, andword="")
-            return words
-        
+            return words        
         else:
             return "Invalid number"
+        
+class DateToAge:
+    def __init__(self):
+        self.convert_number = Convert()
+        
+    
+    ...
             
             
 if __name__ == "__main__":
@@ -20,3 +26,4 @@ if __name__ == "__main__":
     new_words = convert.convert_number(number)
     print(new_words)
     
+
