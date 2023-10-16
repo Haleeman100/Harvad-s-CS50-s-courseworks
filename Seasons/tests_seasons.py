93fa717ec911305420fe3feb180aa9495325d3f2
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2023-10-16 23:37:04 +0100

    added test function to check for invalid numbers

--- Seasons/tests_seasons.py
+++ Seasons/tests_seasons.py
@@ -12,5 +12,11 @@ def age_converter():
 
 def test_valid_numbers(convertion):
     assert convertion.convert_number(3456) == "Three thousand,four hundred fifty six"
+    
+def test_invalid_number(convertion):
+    assert convertion.convert_number(-9) == "Invalid"
+    
+
+    
 
 
