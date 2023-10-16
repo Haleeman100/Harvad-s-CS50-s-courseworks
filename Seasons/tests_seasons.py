8c6f9f7f52deacf52e8b929ae69a8bceafd8bd4e
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2023-10-16 23:35:41 +0100

    added test function to check valid function

--- Seasons/tests_seasons.py
+++ Seasons/tests_seasons.py
@@ -2,4 +2,15 @@ import pytest
 
 from season import Convert, DateToAge
 
+@pytest.fixture
+def convertion():
+    return Convert()
+
+@pytest.fixture
+def age_converter():
+    return DateToAge()
+
+def test_valid_numbers(convertion):
+    assert convertion.convert_number(3456) == "Three thousand,four hundred fifty six"
+
 
