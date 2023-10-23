a3c00945d2a01b6c8f2bf7b93f2530017551d087
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2023-10-23 18:02:30 +0100

    define str and deposit method

--- jar/jar.py
+++ jar/jar.py
@@ -5,14 +5,20 @@ class Jar:
             raise ValueError("Capacity should be a non-negative integer")
         
         self.capacity = capacity
+        self.size = size
         
         ...
 
     def __str__(self):
-        
+        # returns a string of cookies in the jar
+        return "🍪" * self.size       
         ...
 
     def deposit(self, n):
+        # deposit's cookies to jar
+        if n > self.capacity or n + self.size > self.capacity:
+            raise ValueError("Number of cookies should not be higher than that of capacoty ")
+        
         ...
 
     def withdraw(self, n):
