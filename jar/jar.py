7cdbc4a7258babd6af14804d773b584d0d7a0d24
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2023-10-23 18:43:59 +0100

    Hard test working implemented methods

--- jar/jar.py
+++ jar/jar.py
@@ -1,11 +1,11 @@
 class Jar:
     # checks to see if capacity is not positive integer, raises an exception if that's rthe case
-    def __init__(self, capacity=12):
+    def __init__(self, size, capacity = 12):
         if not isinstance(capacity, int) or capacity < 0:
-            raise ValueError("Capacity should be a non-negative integer")
-        
+            raise ValueError("Capacity should be a non-negative integer")   
+        self.size = 0    
         self.capacity = capacity
-        self.size = size
+      
         
         ...
 
@@ -16,7 +16,7 @@ class Jar:
 
     def deposit(self, n):
         # deposit's cookies to jar
-        if n + self.size > self.capacity:
+        if  self.size + n > self.capacity:
             raise ValueError("Number of cookies should not be higher than that of capacity")
         
         self.size += n
@@ -32,10 +32,20 @@ class Jar:
         
         ...
 
-    @property
-    def capacity(self):
-        ...
+    # @property
+    # def capacity(self):
+    #     ...
 
-    @property
-    def size(self):
-        ...
+    # @property
+    # def size(self):
+    #     ...
+        
+def main():
+    jar = Jar(5)
+    jar.deposit(5)
+    jar.withdraw(5)
+    print(jar)
+    
+        
+if __name__ == "__main__":
+    main()
