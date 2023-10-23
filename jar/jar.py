5a57ebd78ee930e5290fed2d942022d69e36d543
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2023-10-23 17:55:15 +0100

    initialised capacity

--- jar/jar.py
+++ jar/jar.py
@@ -1,8 +1,15 @@
 class Jar:
+    # checks to see if capacity is not positive integer, raises an exception if that's rthe case
     def __init__(self, capacity=12):
+        if not isinstance(capacity, int) or capacity < 0:
+            raise ValueError("Capacity should be a non-negative integer")
+        
+        self.capacity = capacity
+        
         ...
 
     def __str__(self):
+        
         ...
 
     def deposit(self, n):
