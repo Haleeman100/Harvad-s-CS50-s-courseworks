09dc1ea1f2deb17d14e0ee82cc6d0df31000d3b3
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2023-10-23 22:56:31 +0100

    add test for withdraw method

--- jar/test_jar.py
+++ jar/test_jar.py
@@ -9,3 +9,8 @@ def check_all():
 def test_deposit(check_all):
     check_all.deposit(3)
     assert check_all.size == 3
+
+def test_withdraw(check_all):
+    check_all.deposit(3)
+    check_all.withdraw(1)
+    assert check_all.size == 2
