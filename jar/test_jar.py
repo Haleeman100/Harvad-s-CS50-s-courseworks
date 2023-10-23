5a607cf0bc56d1b2044460f126f8bcbcfe26747b
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2023-10-23 23:08:25 +0100

    add test for invalid deposit

--- jar/test_jar.py
+++ jar/test_jar.py
@@ -9,8 +9,17 @@ def check_all():
 def test_deposit(check_all):
     check_all.deposit(3)
     assert check_all.size == 3
+    
+def test_invalid_deposit(check_all):
+    check_all.deposit = 5
+    with pytest.raises(ValueError):
+        Jar(check_all) == 3
 
 def test_withdraw(check_all):
     check_all.deposit(3)
     check_all.withdraw(1)
     assert check_all.size == 2
+    
+def test_valid_str(check_all):
+    check_all.deposit(3)
+    assert str(check_all) == "🍪🍪🍪"
