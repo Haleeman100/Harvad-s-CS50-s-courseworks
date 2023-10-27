6bc1a9ded6629666729437a7748e8e6f600dcb51
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2023-10-27 23:00:06 +0100

    completed files

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -12,4 +12,7 @@ pdf.set_font("helvetica", "B", 30)
 pdf.cell(0, 60, 'CS50 Shirtificate', align='C')
 # add image
 pdf.image("shirtificate.png", x = 0, y = 70)
-
+pdf.set_font_size(32)
+pdf.set_text_color(255,255,255)
+pdf.text(x=50, y=150, txt =f"{name} took CS50")
+pdf.output("shirtificate.pdf")
